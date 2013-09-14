2362428b00e36f0ee84baf4dd99827850a846675
charalambosp <papaloizouc@gmail.com>
2013-09-14 01:17:02 +0100

    fancy color printer + PEP8

--- find.py
+++ find.py
@@ -8,24 +8,6 @@ import sys
 import os
 from itertools import starmap
 
-#copied form http://unix.stackexchange.com/questions/148/colorizing-your-terminal-and-shell-environment
-COLOR_NC='\033[0m'
-COLOR_WHITE='\033[1;37m'
-COLOR_BLACK='\033[0;30m'
-COLOR_BLUE='\033[0;34m'
-COLOR_LIGHT_BLUE='\033[1;34m'
-COLOR_GREEN='\033[0;32m'
-COLOR_LIGHT_GREEN='\033[1;32m'
-COLOR_CYAN='\033[0;36m'
-COLOR_LIGHT_CYAN='\033[1;36m'
-COLOR_RED='\033[0;31m'
-COLOR_LIGHT_RED='\033[1;31m'
-COLOR_PURPLE='\033[0;35m'
-COLOR_LIGHT_PURPLE='\033[1;35m'
-COLOR_BROWN='\033[0;33m'
-COLOR_YELLOW='\033[1;33m'
-COLOR_GRAY='\033[0;30m'
-COLOR_LIGHT_GRAY='\033[0;37m'
 
 class FindFiles():
     def _find(self, path:str, regex:str):
@@ -48,10 +30,10 @@ class FindFiles():
 
 class PostFind():
     def execute(self, found_files_list:list, copy_dirs:list, move_dirs:list):
-        self._execute(copy_dirs,found_files_list,self.copy_files)
-        self._execute(move_dirs,found_files_list,self.move_files)
+        self._execute(copy_dirs, found_files_list, self.copy_files)
+        self._execute(move_dirs, found_files_list, self.move_files)
 
-    def _execute(self,dirs:list,found_files_list:list,callback):
+    def _execute(self, dirs:list, found_files_list:list, callback):
         if not dirs:
             return
         args = ((files, dir) for dir in dirs for files in found_files_list)
@@ -101,24 +83,48 @@ class InputHandler():
         setattr(self, attr_name, value)
 
 
-def print_found_files(found_files_list:list):
-    if len(found_files_list[0]) ==0:
-        print("%s%s%s" % ( COLOR_RED , 'No Results Found' , COLOR_NC))
-        return
-    for file_list in found_files_list:
-        for found_file in file_list:
-            print("%s%s%s" % (COLOR_GREEN , found_file , COLOR_NC) )
+class Printer():
+    #copied form http://unix.stackexchange.com/questions/148/colorizing-your-terminal-and-shell-environment
+    COLOR_NC = '\033[0m'
+    COLOR_WHITE = '\033[1;37m'
+    COLOR_BLACK = '\033[0;30m'
+    COLOR_BLUE = '\033[0;34m'
+    COLOR_LIGHT_BLUE = '\033[1;34m'
+    COLOR_GREEN = '\033[0;32m'
+    COLOR_LIGHT_GREEN = '\033[1;32m'
+    COLOR_CYAN = '\033[0;36m'
+    COLOR_LIGHT_CYAN = '\033[1;36m'
+    COLOR_RED = '\033[0;31m'
+    COLOR_LIGHT_RED = '\033[1;31m'
+    COLOR_PURPLE = '\033[0;35m'
+    COLOR_LIGHT_PURPLE = '\033[1;35m'
+    COLOR_BROWN = '\033[0;33m'
+    COLOR_YELLOW = '\033[1;33m'
+    COLOR_GRAY = '\033[0;30m'
+    COLOR_LIGHT_GRAY = '\033[0;37m'
+
+    def _color_str(self, string:str, color:str):
+        return "%s%s%s" % (color, string, self.COLOR_NC)
+
+    def print_found_files(self, found_files_list:list):
+        if len(found_files_list[0]) == 0:
+            print(self._color_str('No Results Found', self.COLOR_RED))
+            return
+        for file_list in found_files_list:
+            for found_file in file_list:
+                print(self._color_str('No Results Found', self.COLOR_GREEN))
 
 
 if __name__ == '__main__':
     inputHandler = InputHandler()
     findFiles = FindFiles()
     postFind = PostFind()
+    printer = Printer()
 
     found_files_list = findFiles.execute(inputHandler.file_name, inputHandler.search_dirs)
     postFind.execute(found_files_list, inputHandler.copy_dirs, inputHandler.move_dirs)
 
-    print_found_files(found_files_list)
+    printer.print_found_files(found_files_list)
 
     # print(COLOR_GREEN + "green" + COLOR_NC )
     # print("\033[1m 999 \033[0m")
